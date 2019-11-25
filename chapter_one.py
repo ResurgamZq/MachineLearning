299189a82b7274c43bcf3a32414eb9ad8c35a7bb
Cyana <2389917927@qq.com>
2019-11-25 19:08:56 +0800

    机器学习第一章

--- chapter_one.py
+++ chapter_one.py
@@ -34,6 +34,7 @@
 # a = np.array([
 
 import scipy as sp
+import matplotlib.pyplot as plt
 data = sp.genfromtxt("data_one\web_traffic.tsv", delimiter="\t")
 # print(data[:30])
 # print(data.shape)
@@ -43,9 +44,36 @@ x = data[:, 0]
 
 # y包含某个小时内的web访问数
 y = data[:, 1]
-print(x)
-print(y)
+# print(x)
+# print(y)
 
 # 有多少小时的数据包含了无效值
+# sp.isnan(y)返回一个布尔型数组，用来表示某个数组项中的内容是否是一个数字
 num = sp.sum(sp.isnan(y))
-print(num)
+# print(num)
+
+# 我们可以使用~在逻辑上对数组取反，使我们可以在x和y中只选择y值合法的项
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+# print(x)
+# print(y)
+
+# 为了获得对数据的第一印象，使用Matplotlib在
+# 散点图上将数据画出来
+plt.scatter(x,y)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)],
+           ['week % i' % w for w in range(10)])
+plt.autoscale(tight=True)
+
+
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
+f1 = sp.poly1d(fp1)
+fx = sp.linspace(0, x[-1], 1000)
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i" % f1.order], loc="upper left")
+plt.grid()
+plt.show()
